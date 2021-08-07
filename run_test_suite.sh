2c626acf451fe4f6ba62564837f5ead68fa8a4ef
egee-irl <brian@egee.io>
2021-08-07 11:50:01 -0700

    dont batch install

--- run_test_suite.sh
+++ run_test_suite.sh
@@ -7,7 +7,7 @@ SUITE_NAME="$1"
 mkdir -p ~/.phoronix-test-suite/test-suites/local/"${SUITE_NAME}"/
 cp -f pts/test-suites/local/"${SUITE_NAME}"/suite-definition.xml ~/.phoronix-test-suite/test-suites/local/"${SUITE_NAME}"/suite-definition.xml
 
-phoronix-test-suite batch-install "${SUITE_NAME}"
+phoronix-test-suite install "${SUITE_NAME}"
 echo "BEGINNING TEST RUN..."
 sleep 5
 phoronix-test-suite batch-run "${SUITE_NAME}"
