9e708db362ae734682201d9e18c166bbd61d96c6
egee-irl <brian@egee.io>
2021-08-07 07:36:29 -0700

    mkdir

--- vps_test.sh
+++ vps_test.sh
@@ -3,7 +3,8 @@ export FORCE_TIMES_TO_RUN=1
 set -eu
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-cp -f pts/test-suites/local/eg-vps/suite-definition.xml ~/.phoronix-test-suite-benchmark/test-suites/local/eg-vps/suite-definition.xml
+mkdir -p ~/.phoronix-test-suite/test-suites/local/eg-vps/
+cp -f pts/test-suites/local/eg-vps/suite-definition.xml ~/.phoronix-test-suite/test-suites/local/eg-vps/suite-definition.xml
 
 phoronix-test-suite batch-install eg-vps
 echo "INFO BEGINNING TEST RUN..."
