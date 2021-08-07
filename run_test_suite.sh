8343301eff88d926e79309d6b03ae4b19b0316f9
egee-irl <brian@egee.io>
2021-08-07 11:37:16 -0700

    just make it a cli

---
+++ run_test_suite.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+export FORCE_TIMES_TO_RUN=1
+set -eu
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+SUITE_NAME="$1"
+
+mkdir -p ~/.phoronix-test-suite/test-suites/local/"${SUITE_NAME}"/
+cp -f pts/test-suites/local/"${SUITE_NAME}"/suite-definition.xml ~/.phoronix-test-suite/test-suites/local/"${SUITE_NAME}"/suite-definition.xml
+
+phoronix-test-suite batch-install "${SUITE_NAME}"
+echo "BEGINNING TEST RUN..."
+sleep 5
+phoronix-test-suite batch-run "${SUITE_NAME}"
